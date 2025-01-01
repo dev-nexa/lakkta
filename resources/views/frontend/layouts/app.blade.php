7e2481a8f0da36de8a35ce63f644d530806770be
anas <kilanyanas2@gmail.com>
2025-01-01 11:23:53 +0300

    fixing pop-up size

--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -295,36 +295,36 @@
     @endphp
     @foreach ($dynamic_popups as $key => $dynamic_popup)
         @if($dynamic_popup->id == 1)
-        <div class="modal website-popup removable-session d-none" data-key="website-popup" data-value="removed">
-            <div class="absolute-full bg-black opacity-60"></div>
-            <div class="modal-dialog modal-dialog-centered modal-dialog-zoom modal-sm mx-4 mx-md-auto" style="margin-bottom: 5rem;">
-                <div class="modal-content position-relative border-0 rounded-0">
-                    <div class="aiz-editor-data">
-                        <div class="d-block">
-                            <img class="w-100" src="{{ uploaded_asset($dynamic_popup->banner) }}" alt="dynamic_popup">
+            <div class="modal website-popup removable-session d-none" data-key="website-popup" data-value="removed">
+                <div class="absolute-full bg-black opacity-60"></div>
+                <div class="modal-dialog modal-dialog-centered modal-dialog-zoom modal-md mx-4 mx-md-auto">
+                    <div class="modal-content position-relative border-0 rounded-0">
+                        <div class="aiz-editor-data">
+                            <div class="d-block">
+                                <img class="w-100" src="{{ uploaded_asset($dynamic_popup->banner) }}" alt="dynamic_popup">
+                            </div>
                         </div>
+                        <div class="pb-5 pt-4 px-3 px-md-2rem">
+                            <h1 class="fs-30 fw-700 text-dark">{{ translate($dynamic_popup->title) }}</h1>
+                            <p class="fs-14 fw-400 mt-3 mb-4">{{ translate($dynamic_popup->summary) }}</p>
+                            @if ($dynamic_popup->show_subscribe_form == 'on')
+                                <form class="" method="POST" action="{{ route('subscribers.store') }}">
+                                    @csrf
+                                    <div class="form-group mb-0">
+                                        <input type="email" class="form-control" placeholder="{{ translate('Your Email Address') }}" name="email" required>
+                                    </div>
+                                    <button type="submit" class="btn btn-block mt-3 rounded-0 text-{{ $dynamic_popup->btn_text_color }}" style="background: {{ $dynamic_popup->btn_background_color }};">
+                                        {{ translate($dynamic_popup->btn_text) }}
+                                    </button>
+                                </form>
+                            @endif
+                        </div>
+                        <button class="absolute-top-right bg-white shadow-lg btn btn-circle btn-icon mr-n3 mt-n3 set-session" data-key="website-popup" data-value="removed" data-toggle="remove-parent" data-parent=".website-popup">
+                            <i class="la la-close fs-20"></i>
+                        </button>
                     </div>
-                    <div class="pb-5 pt-4 px-3 px-md-2rem">
-                        <h1 class="fs-30 fw-700 text-dark">{{ translate($dynamic_popup->title) }}</h1>
-                        <p class="fs-14 fw-400 mt-3 mb-4">{{ translate($dynamic_popup->summary) }}</p>
-                        @if ($dynamic_popup->show_subscribe_form == 'on')
-                            <form class="" method="POST" action="{{ route('subscribers.store') }}">
-                                @csrf
-                                <div class="form-group mb-0">
-                                    <input type="email" class="form-control" placeholder="{{ translate('Your Email Address') }}" name="email" required>
-                                </div>
-                                <button type="submit" class="btn btn-block mt-3 rounded-0 text-{{ $dynamic_popup->btn_text_color }}" style="background: {{ $dynamic_popup->btn_background_color }};">
-                                    {{ translate($dynamic_popup->btn_text) }}
-                                </button>
-                            </form>
-                        @endif
-                    </div>
-                    <button class="absolute-top-right bg-white shadow-lg btn btn-circle btn-icon mr-n3 mt-n3 set-session" data-key="website-popup" data-value="removed" data-toggle="remove-parent" data-parent=".website-popup">
-                        <i class="la la-close fs-20"></i>
-                    </button>
                 </div>
             </div>
-        </div>
         @else
             <div class="modal website-popup removable-session d-none" data-key="website-popup-{{ $dynamic_popup->id }}" data-value="removed">
                 <div class="absolute-full bg-black opacity-60"></div>
